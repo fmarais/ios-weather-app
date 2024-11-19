9099f6779e9011e897c8e316af0821e3f49a53cf
Francois Marais <francois.m@superbalist.com>
2024-11-19 17:32:02 +0200

    Initial Commit

---
+++ Weather App UIKitTests/Weather_App_UIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_UIKitTests.swift
+//  Weather App UIKitTests
+//
+//  Created by francois on 2024/11/19.
+//
+
+import Testing
+@testable import Weather_App_UIKit
+
+struct Weather_App_UIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
